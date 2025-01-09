685e8cc83eaf5d67d69c531f91e09e3880ae33f2
lucaoskaique <lucas.kaique175@hotmail.com>
2025-01-09 03:16:46 -0300

    refactor: update GitHub Actions workflow and improve .gitignore entries

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: process.env.NODE_ENV !== "production" ? undefined : "export",
+  output: 'export', 
   reactStrictMode: true,
   images: {
     unoptimized: true
