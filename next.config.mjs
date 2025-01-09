8f16da2e0728c919411ae080497c1c3162b1da85
lucaoskaique <lucas.kaique175@hotmail.com>
2025-01-09 03:14:41 -0300

    refactor: clean up Next.js configuration and update image handling

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output:  process.env.NODE_ENV !== "production" ? undefined: "export", 
+  output: process.env.NODE_ENV !== "production" ? undefined : "export",
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "pbs.twimg.com",
-        port: "",
-        pathname: "/**"
-      },
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-        port: "",
-        pathname: "/**"
-      },
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        port: "",
-        pathname: "/**"
-      }
-    ]
+    unoptimized: true
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/angel-docs' : '',
+  basePath: process.env.NODE_ENV === "production" ? "/angel-docs" : ""
 }
 
 export default nextConfig
